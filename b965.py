b8db07ccb03da628e5d52f0bb1a3b9ec846af890
Ling-Cheng-Yan <darkness851106@gmail.com>
2024-06-01 23:55:32 +0800

    0601_update

--- b965.py
+++ b965.py
@@ -1,26 +1,25 @@
 r,c,m=map(int,input().split())
 
+
 B = []
 for i in range(r):
     lst = list(map(int,input().split()))
     B.append(lst)
 # print(B)
+# [[1,2,3],[2,4,6]]
 
 M = list(map(int, input().split()))
 
-# while m:
 for mm in range(m-1,-1,-1):
-    # m = m - 1
+    # 翻轉
     if(M[mm]):
         i = 0
-        # j = r -1
-        # while i < j:
         for j in range(r-1,-1,-1):
             if i < j:
                 for x in range(c):
                     B[i][x] , B[j][x] =B[j][x], B[i][x]
             i = i + 1
-            # j = j - 1
+    # 旋轉
     else:
         c_to_r = c - 1
         #A00 = B01
--- g595.py
+++ g595.py
@@ -1,4 +1,5 @@
 count = int(input())
+# print("count=",count)
 wall = list(map(int, input().split()))
 
 sum = 0
@@ -6,9 +7,9 @@ for i in range(1, count-1):
     if wall[i] == 0:
         target = min(wall[i-1], wall[i+1])
         sum += target
-if wall[0] == 0:
+if (wall[0] == 0):
     sum += wall[1]
-if wall[-1] == 0:
+if (wall[-1] == 0):
     sum += wall[-2]
 
 print(sum)
